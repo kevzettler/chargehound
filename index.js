3859579fa3450c784e5d1bb1cf82c514075dc642
kevzettler <kevzettler@gmail.com>
2018-02-15 14:23:30 -0800

    add a 'database'

--- index.js
+++ index.js
@@ -2,75 +2,131 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import next from 'next';
 import stripeInit from 'stripe';
+import chargeHoundInit from 'chargehound';
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 8888;
 const app = next({ dir: '.', dev });
 const handle = app.getRequestHandler()
-const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
 
+const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
+const chargeHound = chargeHoundInit(process.env.CHARGEHOUND_KEY);
+
+const songsDatabase = [
+  {
+    id: '1',
+    song_artist: 'Hound of Love',
+    song_name: 'Rivers Edge',
+    amount: 200,
+  },
+
+  {
+    id: '2',
+    song_artist: 'Hound of Love',
+    song_name: 'Rivers Edge',
+    amount: 500,
+  },
+
+  {
+    id: '3',
+    song_artist: 'Hound of Love',
+    song_name: 'Rivers Edge',
+    amount: 300,
+  },
+
+  {
+    id: '4',
+    song_artist: 'Hound of Love',
+    song_name: 'Rivers Edge',
+    amount: 100,
+  }
+]
 
 app.prepare().then(() => {
   const server = express();
   server.use(bodyParser.json());
 
-  server.post('/api/chargehound-webhook', function (request, response) {
+  server.post('/api/chargehound-webhook', async (request, response) => {
+    console.log('POST /api/chargehound-webhook');
     // Retrieve the request's body and parse it as JSON
     var eventJson = request.body;
 
     // 1) Handle the dispute.created event
     if (!eventJson ||
-        !eventJson.type === 'dispute.created') {
-      return response.send(500);
+        eventJson.type !== 'dispute.created') {
+      return response.sendStatus(500);
     }
 
     // The id of the dispute.
     // The id used by your payment processor is also used by Chargehound.
     var disputeId = eventJson.dispute;
 
-    stripe.disputes.retrieve(disputeId)
-          .then((dispute) => {
-            console.log('fetched dispute from stripe ', disputeId);
-            response.send(200);
-          })
-          .catch((err) => {
-            console.error('failed to fetch a dispute from stripe ', disputeId);
-            response.send(500);
-          });
+    try {
+      const dispute = await stripe.disputes.retrieve(disputeId)
+      console.log('fetched dispute from stripe ', disputeId);
+      console.log(dispute);
+    }catch(err){
+      console.error('failed to fetch a dispute from stripe ', disputeId);
+      console.error(err);
+      return response.sendStatus(500);
+    }
 
-    // 2) Collect your evidence to send to Chargehound.
+    console.log('sending dispute evidence to chargehound...');
+    try{
+      const chres = chargehound.Disputes.submit(disputeId, {
+        template: 'song-purchase',
+        fields: {
+          'purchase_url': 'www.example.com',
+          'song_artist': 'example text',
+          'song_name': 'example text'
+        }
+      });
+      return response.sendStatus(200);
+    }catch(err){
+      console.error('error form chargehound ', 500);
+      return response.sendStatus(500);
+    }
 
-    // 3) POST your evidence to the Chargehound API to respond
+
+
   });
 
-  server.post('/api/charge-stripe-token', (req, res) => {
+  server.post('/api/charge-stripe-token', async (req, res) => {
+    console.log("POST /api/charge-stripe-token");
     const token = req.body.token;
     const item = req.body.item;
 
     console.log(req.body);
 
     if(!token || !token.object){
-      return res.send(500);
+      return res.sendStatus(500);
     }
 
     if(!item || !item.amount){
-      return res.send(500);
+      return res.sendStatus(500);
     }
 
     console.log("charging stripe");
-    stripe.charges.create({
-      amount: item.amount,
-      source: token.id,
-      currency: 'usd',
-      capture: true
-    }).then(function(charge){
-      console.log('stripe charge success!!', charge);
+    try {
+      const charge = await stripe.charges.create({
+        amount: item.amount,
+        source: token.id,
+        currency: 'usd',
+        capture: true,
+        metatdata: item
+      });
+
+      console.log('stripe charge success');
       return res.send(charge);
-    }).catch(function(err){
+    }catch(err){
       console.error('stripe charge error');
-      throw err;
-      return res.send(500);
-    })
+      console.error(err);
+      return res.sendStatus(500);
+    }
+  });
+
+  server.get('/api/songs', (req, res) => {
+    return res.send(JSON.stringify(songsDatabase));
   });
 
   // All get requets get passed to next.js
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import fetch from 'isomorphic-unfetch'
 
-export default class IndexPage extends React.Component{
-  constructor(){
-    super();
-  }
-
+class SongContainer extends React.Component{
   checkoutItem = (amount, artist, song, token) => {
     fetch('/api/charge-stripe-token', {
       method: 'POST',
@@ -28,23 +25,42 @@ export default class IndexPage extends React.Component{
   }
 
   render(){
+    const { song_artist, song_name, amount } = this.props.song;
+    return(
+      <div className="song-container">
+        <p>{song_artist}</p>
+        <p>{song_name}</p>
+        <StripeCheckout
+          token={this.checkoutItem.bind(this,
+                                        500,
+                                        song_artist,
+                                        song_name
+          )}
+          stripeKey="pk_test_ZQJUUIzolNFkfMrHVOoXzRiA"
+          name={`${song_artist} - ${song_name}`}
+          amount={amount}
+          billingAddress={true}
+        >
+          <button>Buy Now</button>
+        </StripeCheckout>
+      </div>
+    )
+  }
+}
+
+export default class IndexPage extends React.Component{
+  static async getInitialProps({ req }) {
+    const res = await fetch(`${req.protocol}://${req.headers.host}/api/songs`);
+    const songs = await res.json();
+    return {songs};
+  }
+
+  render(){
     return (
       <div>
         <h1>Online Music Store</h1>
         <div>
-          <div className="song-record">
-            <p>Artist</p>
-            <p>Title</p>
-            <StripeCheckout
-              token={this.checkoutItem.bind(this, 500, 'Artist', 'Title')}
-              stripeKey="pk_test_ZQJUUIzolNFkfMrHVOoXzRiA"
-              name="Artist Name - Title"
-              amount={500}
-              billingAddress={true}
-            >
-              <button>Buy Now</button>
-            </StripeCheckout>
-          </div>
+          {this.props.songs.map((song, index) => <SongContainer song={song} key={index} />)}
         </div>
       </div>
     );
